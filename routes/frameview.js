0907f5afb4ac241f972750010b156aa081648807
RangerMauve <RangerMauve@hotmail.com>
2013-11-16 10:21:40 -0500

    Fixed frameview pages not caching properly

--- routes/frameview.js
+++ routes/frameview.js
@@ -1,11 +1,12 @@
 var caching = require("../caching"),
-	notmodified = require("notmodified");
+	notmodified = require("notmodified"),
+	path = require("path");
 
 module.exports = function (app) {
 	app.get(/view(\/.+)/, function (req, res) {
 		var path = req.params[0];
 		var name = path.match(/\/([^\/]+).html/)[1];
-		caching.cacheInfo("./templates/frameview.jade", function (err, info) {
+		caching.cacheInfo(path.normalize(__dirname+"/../templates/frameview.jade"), function (err, info) {
 			if (err) return res.send(500, err.stack);
 			if (notmodified(req, res, info)) return res.end();
 			res.render("frameview", {
