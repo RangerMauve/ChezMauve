503f25053d9b282ea5d31e9b363f1e7c6a94bbf5
RangerMauve <RangerMauve@hotmail.com>
2013-11-16 10:02:27 -0500

    Changed caching to use absolute path instead of relative path

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,10 @@
 var caching = require("../caching"),
-	notmodified = require("notmodified");
+	notmodified = require("notmodified"),
+	require("path");
 
 module.exports = function (app) {
 	app.get("/", function (req, res) {
-		caching.cacheInfo("./templates/homepage.jade", function (err, info) {
+		caching.cacheInfo(path.normalize(__dirname+"/../templates/homepage.jade"), function (err, info) {
 			if (err) return res.send(500, err.stack);
 			if (notmodified(req, res, info)) return res.end();
 			res.render("homepage", {
