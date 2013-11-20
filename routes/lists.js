f41e8ceb4cd65c28740d1bf7063b6da918fd3863
RangerMauve <RangerMauve@hotmail.com>
2013-11-20 18:44:57 -0500

    Stripped out library locations into the view locals

--- routes/lists.js
+++ routes/lists.js
@@ -27,3 +27,11 @@ app.get("/games", function (req, res) {
 		res.render("viewlist", require("../data/games.json"));
 	});
 })
+
+app.get("/userscripts",function(req,res){
+	caching.cacheInfo(path.normalize(__dirname+"/../data/userscripts.json"), function (err, info) {
+		if (err) return res.send(500, err.stack);
+		if (notmodified(req, res, info)) return res.end();
+		res.render("viewlist", require("../data/userscripts.json"));
+	});
+});
