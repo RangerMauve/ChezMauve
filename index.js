1c627bcb37e462b911936fa6cae5311e56d32866
RangerMauve <RangerMauve@hotmail.com>
2013-11-25 12:51:29 -0500

    Removed busy module because it's too buggy on RaspberryPi

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var
 	app = express(),
 	logger = require("http-logger").logger,
 	path = require("path"),
-	busy = require("busy")({max:500,interval:200}),
+	//busy = require("busy")(),
 	http = require('http'),
 	server = http.createServer(app);
 
@@ -17,10 +17,10 @@ app.locals.pretty = true;
 app.locals.menu = require("./data/menu.json");
 app.locals.scripts = require("./data/scriptLocals.json").development;
 
-// Prevent server for crashing too hard under high load
+/*// Prevent server for crashing too hard under high load
 app.use(function(req,res,next){
 	busy.blocked ? res.send(503,"Server load too high") : next();
-});
+});*/
 
 // Parsing out the body and session data
 app.use(express.json());
