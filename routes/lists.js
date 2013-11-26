2f0c6d2ba44b9376923a69932796449f6236584a
RangerMauve <RangerMauve@hotmail.com>
2013-11-25 19:44:13 -0500

    Removed caching because it was buggy

--- routes/lists.js
+++ routes/lists.js
@@ -1,37 +1,18 @@
-var caching = require("../caching"),
-	notmodified = require("notmodified"),
-	path = require("path"),
+var path = require("path"),
 	app = require("../").app;
 
 app.get("/tools", function (req, res) {
-	// Set caching for this route based on the template since the data is static
-	caching.cacheInfo(path.normalize(__dirname + "/../data/tools.json"), function (err, info) {
-		if (err) return res.send(500, err.stack);
-		if (notmodified(req, res, info)) return res.end();
-		res.render("viewlist", require("../data/tools.json"))
-	})
+	res.render("viewlist", require("../data/tools.json"))
 });
 
 app.get("/", function (req, res) {
-	caching.cacheInfo(path.normalize(__dirname + "/../data/home.json"), function (err, info) {
-		if (err) return res.send(500, err.stack);
-		if (notmodified(req, res, info)) return res.end();
-		res.render("homepage", require("../data/home.json"));
-	});
-})
+	res.render("homepage", require("../data/home.json"));
+});
 
 app.get("/games", function (req, res) {
-	caching.cacheInfo(path.normalize(__dirname+"/../data/games.json"), function (err, info) {
-		if (err) return res.send(500, err.stack);
-		if (notmodified(req, res, info)) return res.end();
-		res.render("viewlist", require("../data/games.json"));
-	});
-})
+	res.render("viewlist", require("../data/games.json"));
+});
 
-app.get("/userscripts",function(req,res){
-	caching.cacheInfo(path.normalize(__dirname+"/../data/userscripts.json"), function (err, info) {
-		if (err) return res.send(500, err.stack);
-		if (notmodified(req, res, info)) return res.end();
-		res.render("viewlist", require("../data/userscripts.json"));
-	});
+app.get("/userscripts", function (req, res) {
+	res.render("viewlist", require("../data/userscripts.json"));
 });
