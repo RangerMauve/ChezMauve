d89319a46cc5dbaeb801e73485eafc6c7c95d9a4
RangerMauve <RangerMauve@hotmail.com>
2013-11-27 19:44:06 -0500

    Moving templates to seperate blog stuff from rest of site

--- routes/lists.js
+++ routes/lists.js
@@ -2,25 +2,21 @@ var path = require("path"),
 	app = require("../").app;
 
 app.get("/tools", function (req, res) {
-	res.render("viewlist", require("../data/tools.json"))
-});
-
-app.get("/", function (req, res) {
-	res.render("homepage", require("../data/home.json"));
+	res.render("blog/viewlist", require("../data/tools.json"))
 });
 
 app.get("/games", function (req, res) {
-	res.render("viewlist", require("../data/games.json"));
+	res.render("blog/viewlist", require("../data/games.json"));
 });
 
 app.get("/userscripts", function (req, res) {
-	res.render("viewlist", require("../data/userscripts.json"));
+	res.render("blog/viewlist", require("../data/userscripts.json"));
 });
 
 app.get(/view(\/.+)/, function (req, res) {
 	var path = req.params[0];
 	var name = path.match(/\/([^\/]+).html/)[1];
-	res.render("frameview", {
+	res.render("blog/frameview", {
 		title: name,
 		view: {
 			url: path
