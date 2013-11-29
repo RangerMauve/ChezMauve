920a5a16620c1be9b72451d2edbd260599a58166
RangerMauve <RangerMauve@hotmail.com>
2013-11-28 19:29:43 -0500

    Implemented login via google

--- index.js
+++ index.js
@@ -6,24 +6,43 @@ var express = require("express"),
 	logger = require("http-logger").logger,
 	path = require("path"),
 	http = require('http'),
-	server = http.createServer(app);
+	server = http.createServer(app),
+	config;
 
 module.exports.app = app;
 module.exports.server = server;
 
 // Configure production-specific settings
 app.configure("production", function () {
+	// Set up config
+	var rawcfg = require("./config.json");
+	config = rawcfg.production;
+	for (var k in rawcfg) {
+		if (k !== "production" && k != "development") {
+			config[k] = rawcfg[k];
+		}
+	}
+
 	// Set script locals to not be pulled from file system
 	app.locals.scripts = require("./data/scriptLocals.json").production;
 });
 
 // Configure development-specific settings
 app.configure("development", function () {
+	// Set up config
+	var rawcfg = require("./config.json");
+	config = rawcfg.development;
+	for (var k in rawcfg) {
+		if (k !== "production" && k != "development") {
+			config[k] = rawcfg[k];
+		}
+	}
+
 	// Have script locals point to files on the server for use without an internet connection
 	app.locals.scripts = require("./data/scriptLocals.json").development;
 
 	// Have logging made to the console
-	app.use(express.logger());
+	//app.use(express.logger());
 });
 
 // Configure for both production and development
@@ -46,28 +65,51 @@ app.configure("production", "development", function () {
 	// Logging
 	app.use(logger(path.join(__dirname, "httpRequests.log")));
 
-	// Compression
-	app.use(express.compress());
-
 	// Configure passport.js
+	app.use(passport.initialize());
+	app.use(passport.session());
+	passport.serializeUser(function (user, done) {
+		console.log("Serialize", user);
+		done(null, user.openId);
+	});
+	passport.deserializeUser(function (id, done) {
+		console.log("Deserialize", id);
+		db.Users.findOne({
+			openId: id
+		}, done);
+	})
 	passport.use(new GoogleStrategy({
-
+			returnURL: config.domain + "/auth/google/return",
+			realm: config.domain + "/"
 		},
-		function (id, profile, done) {
-
+		function (ident, profile, done) {
+			var url = require("url");
+			var id = url.parse(ident, true).query.id;
+			console.log("Login", profile);
+			db.Users.update({
+				openId: id
+			}, {
+				openId: id,
+				profile: profile
+			}, {
+				upsert: true
+			}, function (err, docs) {
+				db.Users.findOne({
+					openId: id
+				}, function (err, user) {
+					done(err, user);
+				});
+			});
 		}
 	));
+
+	// Compression
+	app.use(express.compress());
 
 	// Serving public files
 	require("./routes");
 	app.use(express.static(__dirname + "/public"));
 	app.use(express.directory('public'))
-
-	// For pesky bots scanning stuff they shouldn't be
-	app.use(function (req, res, next) {
-		res.writeHead(418);
-		res.end("<html>\n<center><h1>404</h1></center>\n<!-- What are you even looking here for?-->\n</html>");
-	});
 });
 
 server.listen(process.env.VCAP_APP_PORT || 80);
--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,16 @@
-var app = require("../").app;
-var Users = require("../data").Users;
+var app = require("../").app,
+	passport = require("passport"),
+	Users = require("../data").Users;
 
-app.get("/login",function(req,res){
-	res.render("login",{});
+app.get("/login", function (req, res) {
+	res.render("login", {});
 });
+
+app.get("/auth/google", passport.authenticate("google"));
+
+app.get('/auth/google/return',
+	passport.authenticate('google', {
+		successRedirect: '/',
+		failureRedirect: '/login'
+	})
+);
