2e8487f9013832351c9de542393b9dbacdfa8e2b
RangerMauve <RangerMauve@hotmail.com>
2013-12-02 15:06:59 -0500

    Added HashBeGone userscript

---
+++ public/userscripts/HashBeGone.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name	Hash Be Gone
+// @namespace	http://mauve.us.to
+// @description	Do you hate #hashtags on the internet? Well this gets rid of all of them.
+// @include	*
+// ==/UserScript==
+
+// Wrapper to keep things in their own scope
+(function(){
+
+// Gets all the text nodes from a given DOM node
+function getTextNodes(from){
+	var n, a=[], w=document.createTreeWalker(from,NodeFilter.SHOW_TEXT,null,false);
+	while(n=w.nextNode())a.push(n);
+	return a;
+}
+
+// Processes an array of TextNodes and performs substitution on the text they contain
+function substitute(a){
+	a.forEach(function(e){
+		e.nodeValue=e.nodeValue.replace(/#\S+/g,"");
+	});
+}
+
+// Do the substitution on everything at page load
+substitute(getTextNodes(document.body));
+
+// Perform the substitution whenever a new node is added to support AJAX sites
+document.body.addEventListener("DOMNodeInserted",function(e){
+	substitute(getTextNodes(e.target));
+});
+
+})();
