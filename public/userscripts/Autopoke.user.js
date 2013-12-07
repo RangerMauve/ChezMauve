0681212f1dc7b79dbab82044a23e17759b09f259
RangerMauve <RangerMauve@hotmail.com>
2013-12-07 08:19:55 -0500

    Fix typo in Autopoke

--- public/userscripts/Autopoke.user.js
+++ public/userscripts/Autopoke.user.js
@@ -6,7 +6,7 @@
 // ==/UserScript==
 (function(){
 	function pokemon(){
-		var as= document.querySelectorAll(".mam .pam");
+		var as= document.querySelectorAll(".mam.pam");
 		for(var i =0,j=as.length; i < j; ++i)
 			if(as[i].id){
 				var e = as[i];
